8028fe3701fd28bf4182123680fcc6195ed754dc
Christian Grobmeier <cg@grobmeier.de>
2018-03-20 08:20:28 +0100

    added tasks

--- src/main/java/solutions/grobmeier/caf/App.java
+++ src/main/java/solutions/grobmeier/caf/App.java
@@ -1,13 +1,36 @@
 package solutions.grobmeier.caf;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class App {
+
+	// TODO: should work as parameter
+	private static final String FILENAME = "mattygroves.txt";
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Reading file");
+
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		fr = new FileReader(FILENAME);
+		br = new BufferedReader(fr);
+
+		String mattyGrovesLyricsLine;
+		while ((mattyGrovesLyricsLine = br.readLine()) != null) {
+			LyricsLine.line = mattyGrovesLyricsLine;
+			LyricsLine.enableLineNumbers(); // TODO: does not work
+			LyricsLine.printStuff();
+		}
+	
+		try {
+			if (br != null)
+				br.close();
+			if (fr != null)
+				fr.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
---
+++ src/main/java/solutions/grobmeier/caf/LyricsLine.java
@@ -0,0 +1,13 @@
+package solutions.grobmeier.caf;
+
+public class LyricsLine {
+	public static String line;
+	
+	public static void printStuff() {
+		System.out.println(line);
+	}
+	
+	public static void enableLineNumbers() {
+		// TODO print line when this method was called
+	}
+}
